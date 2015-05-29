028a13b3993893d6125f5d41a9d17cf11874f75f
Constanti <constantin.dumba@gmail.com>
2015-05-28 19:42:51 -1000

    optimized function, it works

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,36 +1,13 @@
 exports.largestPrimeFactor = largestPrimeFactor;
 function largestPrimeFactor(n){
-  //var primeNumber = 0;
-  var factor = 0;
-  var count = 0;
-  var isNotPrime = false;
-  for (var i = n-1; i > 1; i--) {
-    debugger;
-    if(n%i===0){
-      factor = i;
-      for(var j = factor-1; j>1; j--){
-        if(factor%j===0){
-          isNotPrime = true;
-        }
+  var x=2;
+  while(n !==1){
+    if(n%x===0){
+      if(n/x > 1){
+        n = n/x;
       }
-      if(!isNotPrime){
-        return factor;
-      }
-      else isNotPrime = false;
+      else return x;
     }
-
+    else x++;
   }
-  // do your work here
-
-  //return primeNumber;
-};
-largestPrimeFactor(13195);
-
-
-// var testPrime = function(number){
-//   for (var i = number - 1; i > 1; i--) {
-//     if(number%i===0){
-//       return
-//     }
-//   };
-// }
+}
